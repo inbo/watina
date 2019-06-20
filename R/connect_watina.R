9fad9dbf599ce0bf82318bcb8d2cca155c26a534
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-20 14:13:50 +0200

    Function connect_watina() added

---
+++ R/connect_watina.R
@@ -0,0 +1,25 @@
+#' Connect to the INBO Watina database
+#'
+#' Returns a connection to the INBO \strong{Watina} database.
+#' The function can only be used from within the INBO network.
+#'
+#' Don't forget to disconnect at the end of your R-script using
+#' \code{\link[DBI:dbDisconnect]{DBI::dbDisconnect()}}!
+#'
+#' @return
+#' A \code{DBIConnection} object.
+#'
+#' @examples
+#' \dontrun{
+#' watina <- connect_watina()
+#' # Do your stuff.
+#' # Disconnect:
+#' DBI::dbDisconnect(watina)
+#' }
+#'
+#' @export
+#' @importFrom inborutils connect_inbo_dbase
+connect_watina <- function() {
+    connect_inbo_dbase("W0002_00_Watina")
+}
+
