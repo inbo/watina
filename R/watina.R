090fc84c49941a2fb5db74d8d073d774b007eada
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-24 16:09:36 +0200

    Add package documentation file *
    This was especially done to declare a global variable, used in get_locs().

---
+++ R/watina.R
@@ -0,0 +1,12 @@
+#' \code{watina} package
+#'
+#' Querying & preprocessing data from the INBO Watina database (mainly groundwater data)
+#'
+#' See the README on
+#' \href{https://github.com/inbo/watina/blob/master/README.md}{GitHub}.
+#'
+#' @docType package
+#' @name watina
+NULL
+
+utils::globalVariables(c("."))
