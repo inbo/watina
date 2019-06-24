cce03459a5965b741915d9d447cc5d75cc60dc23
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-24 16:12:01 +0200

    Add 2 functions get_locs() and as_points()

---
+++ R/get.R
@@ -0,0 +1,244 @@
+#' Get locations from the database
+#'
+#' Returns locations from the \emph{Watina} database that meet
+#' several criteria (spatial or non-spatial), either as a lazy object or as a
+#' local tibble.
+#' Essential metadata are included in the result.
+#'
+#' (TO BE ADDED: Explanation on the variable names of the returned object,
+#' and on the different available values of loc_type and loc_validity)
+#'
+#' @param con A \code{DBIConnection} object to Watina.
+#' See \code{\link{connect_watina}} to generate one.
+#' @param max_filterdepth Numeric.
+#' Maximum depth of the filter bottom below soil surface, as meters.
+#' This condition is only applied to piezometers.
+#' @param mask An optional geospatial filter of class \code{sf}.
+#' If provided, only locations that intersect with \code{mask} will be returned.
+#' The CRS must be Belgian Lambert 72 (EPSG-code
+#' \href{https://epsg.io/31370}{31370}).
+#' @param bbox Optional geospatial fiter (rectangle).
+#' A bounding box (class \code{bbox}), or a vector of four named elements
+#' \code{xmin}, \code{xmax}, \code{ymin}, \code{ymax} defining the
+#' boundary coordinates of a bounding box.
+#' If provided, only locations within this rectangular area will be returned.
+#' The CRS must be Belgian Lambert 72 (EPSG-code
+#' \href{https://epsg.io/31370}{31370}).
+#' @param area_codes An optional vector with area codes.
+#' If provided, only locations within the areas will be returned.
+#' @param loc_type Type of the location (mainly: the type of measurement device).
+#' Defaults to \code{"P"}, i.e. only groundwater piezometers are returned by
+#' default.
+#' @param loc_validity Validation status of the location.
+#' Defaults to \code{"VLD"}, i.e. only validated locations are returned by
+#' default.
+#' @param loc_vec An optional vector with location codes.
+#' If provided, only locations are returned that are present in this vector.
+#' @param collect Should the data be retrieved as a local tibble?
+#' If \code{FALSE} (the default), a \code{tbl_lazy} object is returned
+#' (lazy query).
+#' Hence the result can be further built upon before retrieving data with
+#' \code{\link[dplyr:collect]{collect()}}.
+#'
+#' @return
+#' By default, a \code{tbl_lazy} object.
+#' With \code{collect = FALSE} or with a specified \code{mask},
+#' a local \code{\link[tibble]{tibble}} is returned.
+#'
+#' @examples
+#' \dontrun{
+#' watina <- connect_watina()
+#'
+#' get_locs(watina,
+#'          bbox = c(xmin = 1.4e+5,
+#'                   xmax = 1.7e+5,
+#'                   ymin = 1.6e+5,
+#'                   ymax = 1.9e+5))
+#'
+#' get_locs(watina,
+#'          area_codes = c("KAL", "KBR"),
+#'          collect = TRUE)
+#'
+#' get_locs(watina,
+#'          area_codes = c("KAL", "KBR"),
+#'          loc_type = c("P", "S"),
+#'          collect = TRUE)
+#'
+#' # Mark the different output of:
+#'   get_locs(watina,
+#'            loc_vec = c("KBRP081", "KBRP090", "KBRP095", "KBRS001"),
+#'            loc_type = c("P", "S"),
+#'            collect = TRUE)
+#'   # versus:
+#'   get_locs(watina,
+#'            loc_vec = c("KBRP081", "KBRP090", "KBRP095", "KBRS001"),
+#'            collect = TRUE)
+#'
+#' # Selecting all piezometers with status VLD or ENT of the
+#' # province "West-Vlaanderen:
+#' data(BE_ADMIN_PROVINCE,
+#'      package = "BelgiumMaps.StatBel")
+#' library(dplyr)
+#' library(sf)
+#' mymask <-
+#'     st_as_sf(BE_ADMIN_PROVINCE) %>%
+#'     filter(str_detect(TX_PROV_DESCR_NL, "West")) %>%
+#'     st_transform(crs = 31370)
+#' get_locs(watina,
+#'          loc_validity = c("VLD", "ENT"),
+#'          mask = mymask)
+#'
+#' # Disconnect:
+#' DBI::dbDisconnect(watina)
+#' }
+#'
+#' @export
+#' @importFrom inborutils connect_inbo_dbase
+#' @importFrom rlang .data
+#' @importFrom assertthat
+#' assert_that
+#' is.number
+#' @importFrom sf
+#' st_drop_geometry
+#' st_intersects
+#' @importFrom dplyr
+#' tbl
+#' filter
+#' left_join
+#' select
+#' collect
+#' distinct
+get_locs <- function(con,
+                     max_filterdepth = 3,
+                     mask = NULL,
+                     bbox = NULL,
+                     area_codes = NULL,
+                     loc_type = c("P", "S", "R", "N", "W", "D", "L", "B"),
+                     loc_validity = c("VLD", "ENT", "DEL", "CLD"),
+                     loc_vec = NULL,
+                     collect = FALSE) {
+
+    assert_that(is.number(max_filterdepth))
+    assert_that(is.null(bbox) | all(sort(names(bbox)) ==
+                                        c("xmax", "xmin", "ymax", "ymin")),
+                msg = "You did not correctly specify bbox.")
+    assert_that(is.null(area_codes) | all(is.character(area_codes)))
+    assert_that(is.null(loc_vec) | all(is.character(loc_vec)),
+                msg = "loc_vec must be a character vector.")
+    assert_that(is.logical(collect))
+
+    if (!is.null(mask) & !collect) {
+        message("As a mask always invokes a collect(), the argument 'collect = FALSE' will be ignored.")
+    }
+
+    if (missing(loc_type)) {
+        loc_type <- match.arg(loc_type)} else {
+            assert_that(all(loc_type %in%
+                                c("P", "S", "R", "N", "W", "D", "L", "B")),
+                        msg = "You specified at least one unknown loc_type.")
+        }
+
+    if (missing(loc_validity)) {
+        loc_validity <- match.arg(loc_validity)} else {
+            assert_that(all(loc_validity %in%
+                                c("VLD", "ENT", "DEL", "CLD")),
+                        msg = "You specified at least one unknown loc_validity.")
+        }
+
+    locs <-
+        tbl(con, "vwDimMeetpunt") %>%
+        filter(.data$MeetpuntTypeCode %in% loc_type,
+               .data$MeetpuntStatusCode %in% loc_validity
+               ) %>%
+        left_join(tbl(con, "vwDimGebied") %>%
+                      select(.data$GebiedWID,
+                             .data$GebiedCode,
+                             .data$GebiedNaam))
+
+    if (!is.null(loc_vec)) {
+        locs <-
+            locs %>%
+            filter(.data$MeetpuntCode %in% loc_vec)
+    }
+
+    if (!is.null(area_codes)) {
+        locs <-
+            locs %>%
+            filter(.data$GebiedCode %in% area_codes)
+    }
+
+    if (!is.null(bbox)) {
+        bbox_xmin <- unname(bbox["xmin"])
+        bbox_xmax <- unname(bbox["xmax"])
+        bbox_ymin <- unname(bbox["ymin"])
+        bbox_ymax <- unname(bbox["ymax"])
+        locs <-
+            locs %>%
+            filter(.data$MeetpuntXCoordinaat >= bbox_xmin,
+                   .data$MeetpuntXCoordinaat <= bbox_xmax,
+                   .data$MeetpuntYCoordinaat >= bbox_ymin,
+                   .data$MeetpuntYCoordinaat >= bbox_ymax)
+    }
+
+    locs <-
+        locs %>%
+        left_join(tbl(con, "vwDimPeilpunt") %>%
+                      select(.data$MeetpuntWID,
+                             .data$PeilpuntStatusCode,
+                             .data$PeilbuisLengte,
+                             .data$ReferentieNiveauMaaiveld) %>%
+                      filter(.data$PeilpuntStatusCode %in% c("VLD",
+                                                       "ENT",
+                                                       "CLD"))) %>%
+        filter(.data$MeetpuntTypeCode == "P" &
+                   (.data$PeilbuisLengte - .data$ReferentieNiveauMaaiveld) <=
+                     max_filterdepth |
+                   .data$MeetpuntTypeCode != "P"
+               ) %>%
+        select(loc_wid = .data$MeetpuntWID,
+               loc_code = .data$MeetpuntCode,
+               area_code = .data$GebiedCode,
+               area_name = .data$GebiedNaam,
+               x = .data$MeetpuntXCoordinaat,
+               y = .data$MeetpuntYCoordinaat,
+               loc_validitycode = .data$MeetpuntStatusCode,
+               loc_validity = .data$MeetpuntStatus,
+               loc_typecode = .data$MeetpuntTypeCode,
+               loc_type = .data$MeetpuntType) %>%
+        distinct
+
+    if (!is.null(mask)) {
+
+        nr_dropped_locs <-
+            locs %>%
+            filter(is.na(.data$x) | is.na(.data$y)) %>%
+            collect() %>%
+            nrow
+
+        if (nr_dropped_locs > 0) {
+            warning("Dropped ",
+                    nr_dropped_locs,
+                    " locations from which x or y coordinates were missing.")
+        }
+
+        locs <-
+            locs %>%
+            collect %>%
+            filter(!is.na(.data$x), !is.na(.data$y)) %>%
+            as_points %>%
+            filter(st_intersects(x = .,
+                                 y = mask,
+                                 sparse = FALSE)) %>%
+            st_drop_geometry
+
+    }
+
+    if (collect) {
+        locs <-
+            locs %>% collect
+    }
+
+    return(locs)
+
+}
+
---
+++ R/sf.R
@@ -0,0 +1,56 @@
+#' Convert a dataframe with X and Y coordinates to a geospatial points object
+#'
+#' \code{as.points} is a convenience function which accepts as input a dataframe
+#' with X/Y coordinates (in meters), assumed to come from the coordinate
+#' reference system (CRS)
+#' 'Belge 1972 / Belgian Lambert 72' (EPSG \href{https://epsg.io/31370}{31370}).
+#' It converts the dataframe into an \code{sf} points object in the same CRS.
+#'
+#' As locations in Watina are typically defined by their X/Y coordinates,
+#' this function eases the conversion to spatial data.
+#' To later remove all spatial information from the result, you can use
+#' \code{\link[sf:st_drop_geometry]{sf::st_drop_geometry()}}.
+#'
+#' @param df A dataframe with X and Y coordinates in meters, assumed to be in
+#' the Belgian Lambert 72 CRS (EPSG-code 31370).
+#' @param xvar String. The X coordinate variable name. Defaults to \code{"x"}.
+#' @param yvar String. The Y coordinate variable name. Defaults to \code{"y"}.
+#' @param remove Logical. Should the X and Y coordinates be removed from the
+#' dataframe after conversion to a spatial object?
+#'
+#' @return
+#' An \code{sf} object of geometry type \code{POINT} and with EPSG-code
+#' 31370 as coordinate reference system.
+#'
+#' @examples
+#' library(tibble)
+#' mydata <-
+#'   tibble(
+#'     a = runif(5),
+#'     x = rnorm(5, 155763, 5),
+#'     y = rnorm(5, 132693, 5)
+#'   )
+#' as_points(mydata)
+#'
+#' @export
+#' @importFrom sf st_as_sf
+#' @importFrom assertthat
+#' assert_that
+#' is.string
+#' has_name
+#' @importFrom dplyr
+#' %>%
+as_points <- function(df, xvar = "x", yvar = "y", remove = FALSE) {
+
+    assert_that(inherits(df, "data.frame"))
+    assert_that(is.string(xvar))
+    assert_that(is.string(yvar))
+    assert_that(has_name(df, xvar))
+    assert_that(has_name(df, yvar))
+
+    df %>%
+        st_as_sf(coords = c(xvar, yvar),
+                 crs = 31370,
+                 remove = remove)
+
+}
