91ce76df7c6829a630e4f7982d40607c51ad2a24
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-28 13:32:39 +0200

    Split internal function into filter_xg3() & qualify_xg3()

--- R/eval.R
+++ R/eval.R
@@ -10,10 +10,10 @@
 #' If more than one XG3 type is requested, or if \code{data} contains both
 #' vertical CRSes, an extra value "\code{combined}" is listed in the
 #' \code{xg3_variable} column.
-#' It evaluates the combined presence of the involved XG3 variables at each
+#' It evaluates the combined presence of the selected XG3 variables at each
 #' location.
 #'
-#' @inheritParams qualify_xg3
+#' @inheritParams filter_xg3
 #'
 #' @return
 #' A tibble with variables \code{loc_code} (see \code{\link{get_locs}}),
@@ -80,46 +80,35 @@ eval_xg3_avail <- function(data,
 
 
 
-#' Qualify XG3 data per location x hydroyear
+
+
+
+
+
+#' Filter XG3 data per location x hydroyear
 #'
-#' Helper function to select the specified XG3 columns and determine
-#' their availability per location x hydroyear.
-#'
-#' The column \code{xg3_variable} in the resulting tibble
-#' lists the requested XG3 types, for each location and hydroyear.
-#' If more than one XG3 type is requested, or if \code{data} contains both
-#' vertical CRSes, an extra value "\code{combined}" is listed in the
-#' \code{xg3_variable} column.
-#' It evaluates the combined presence of the involved XG3 variables at each
-#' location and hydroyear.
+#' Helper function to select the specified XG3 columns per location x hydroyear.
 #'
 #' @param data An object returned by \code{\link{get_xg3}}.
 #' @param xg3_type Character vector of length 1, 2 or 3.
-#' Defines the types of XG3 which are taken to evaluate the hydroyears for
-#' each location.
+#' Defines the types of XG3 which are taken from \code{data}.
 #' Specifies the 'X' in 'XG3': either \code{"L"}, \code{"H"} and/or \code{"V"}.
 #'
 #' @return
-#' A tibble, with columns \code{xg3_variable}
-#' (character; see Details) and \code{available} (logical) to denote at each
-#' location whether the hydroyear has the requested XG3 values available.
+#' A \code{tbl_lazy} object or a tibble, which is like \code{data} but with
+#' non-requested XG3 variables discarded.
 #'
 #' @keywords internal
 #' @importFrom assertthat
 #' assert_that
 #' @importFrom rlang .data
-#' @importFrom tidyr
-#' gather
 #' @importFrom dplyr
 #' %>%
 #' select
-#' collect
 #' contains
 #' arrange
 #' filter
-#' vars
-#' mutate_at
-qualify_xg3 <- function(data,
+filter_xg3 <- function(data,
                         xg3_type) {
 
     if (missing(xg3_type)) {
@@ -139,9 +128,9 @@ qualify_xg3 <- function(data,
                 msg = paste("xg3_type is set to",
                             dput(xg3_type),
                             "but at least one XG3 type is missing in data.")
-                )
+    )
 
-    xg3_qualified <-
+    xg3_filtered <-
         data %>%
         select(.data$loc_code,
                .data$hydroyear,
@@ -151,6 +140,62 @@ qualify_xg3 <- function(data,
         ) %>%
         arrange(.data$loc_code,
                 .data$hydroyear)
+
+    return(xg3_filtered)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Qualify XG3 data per location x hydroyear
+#'
+#' Helper function to determine
+#' the availability of the specified XG3 types per location x hydroyear.
+#'
+#' The column \code{xg3_variable} in the resulting tibble
+#' lists the requested XG3 types, for each location and hydroyear.
+#' If more than one XG3 type is requested, or if \code{data} contains both
+#' vertical CRSes, an extra value "\code{combined}" is listed in the
+#' \code{xg3_variable} column.
+#' It evaluates the combined presence of all selected XG3 variables at each
+#' location and hydroyear.
+#'
+#' @inheritParams filter_xg3
+#'
+#' @return
+#' A tibble, with columns \code{xg3_variable}
+#' (character; see Details) and \code{available} (logical) to denote at each
+#' location whether the hydroyear has the requested XG3 values available.
+#'
+#' @keywords internal
+#' @importFrom rlang .data
+#' @importFrom tidyr
+#' gather
+#' @importFrom dplyr
+#' %>%
+#' select
+#' collect
+#' contains
+#' vars
+#' mutate_at
+qualify_xg3 <- function(data,
+                        xg3_type) {
+
+    xg3_qualified <-
+        data %>%
+        filter_xg3(xg3_type)
 
     if (inherits(xg3_qualified, "tbl_lazy")) {
         xg3_qualified <- collect(xg3_qualified)
@@ -219,7 +264,7 @@ qualify_xg3 <- function(data,
 #' vertical CRSes) and \code{xg3_type} results in more than one requested
 #' variable.
 #' This 'combined' level defines an XG3 series as an XG3 series where each
-#' 'member' year has \strong{all} individual XG3 variables available.
+#' 'member' year has \strong{all} selected XG3 variables available.
 #'
 #' @param max_gap A positive integer (can be zero).
 #' It is part of what the user defines as 'an XG3 series':
@@ -231,7 +276,7 @@ qualify_xg3 <- function(data,
 #' i.e. the time (expressed as years)
 #' from the first to the last year of the XG3 series.
 #'
-#' @inheritParams qualify_xg3
+#' @inheritParams filter_xg3
 #'
 #' @return
 #' A tibble with variables:
