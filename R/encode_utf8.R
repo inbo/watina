fa3e816b61111d533a948557f1fe1bf8c8d115bd
florisvdh <floris.vanderhaeghe@inbo.be>
2019-10-24 09:50:35 +0200

    Add function convertdf_utf8()

---
+++ R/encode_utf8.R
@@ -0,0 +1,48 @@
+#' Convert encoding of character and factor variables in a dataframe
+#'
+#'
+#'
+#' @param x A dataframe.
+#' @param to Encoding string to convert to.
+#' All \code{R} platforms support \code{""} (for the encoding of the current
+#' locale), \code{"latin1"} and \code{"UTF-8"}.
+#' See \code{\link{iconv}} for more information.
+#'
+#' @return
+#' The dataframe is returned with character variables (and levels of
+#' (character) factor variables) converted to the specified encoding.
+#'
+#' @export
+#' @importFrom dplyr
+#' %>%
+#' mutate_if
+#' @importFrom assertthat
+#' assert_that
+#' is.string
+#' @importFrom rlang .data
+convertdf_utf8 <- function(x,
+                           to = "UTF-8") {
+
+    assert_that(inherits(x, "data.frame"))
+    assert_that(is.string(to))
+
+    is_chfact <- function(vec) {
+            if (is.factor(x)) {
+            is.character(levels(vec))
+        } else FALSE
+    }
+
+    conv_levels <- function(fact, to) {
+        levels(fact) <- iconv(levels(fact),
+                              to = to)
+    }
+
+    x %>%
+        mutate_if(is.character,
+                  iconv,
+                  to = to) %>%
+        mutate_if(is_chfact,
+                  conv_levels,
+                  to = to)
+
+}
