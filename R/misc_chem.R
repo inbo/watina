df983bec81d3660d76f890e4762a75823a13f807
cecileherr <cecile.herr@inbo.be>
2026-02-24 09:43:45 +0100

    misc_chem: adapt calculate_ir
    - calc_ir => calculate_ir
    - add checks for the input
    - make the function more generic (this version only requires a few fields instead of all fields from get_chem)

--- R/misc_chem.R
+++ R/misc_chem.R
@@ -1,7 +1,7 @@
 #' Compute the ionic ratio
 #'
-#' Uses the output of \code{\link{get_chem}} and convert it to a new tibble with
-#' the ionic ratio (and the right input format to make a Van Wirdum diagram).
+#' Calculate the ionic ratio of a water sample based on the output of
+#' \code{\link{get_chem}} and add it to the input data as a new column \code{ir}.
 #'
 #' Depending on the units of the Ca and Cl concentrations, the ionic ratio gets
 #' calculated as follows:
@@ -17,13 +17,17 @@
 #'
 #' The result is the ionic ratio \code{ir} without units (0-1).
 #'
-#' @param x Output of the \code{get_chem} function (hydrochemical data retrieved
-#' from Watina). It can be a lazy object as well as a local object
+#' @param data Output of the \code{get_chem} function (hydrochemical data
+#' retrieved from Watina) or any dataset with at least the columns
+#' \code{loc_code},
+#' \code{chem_variable} (with at least concentrations for Ca and Cl),
+#' \code{value}, \code{unit} and \code{date}.
+#' It can be a lazy object as well as a local object
 #' \code{(get_chem(collect = TRUE))}.
 #'
 #' @return
-#' A tibble similar to x but with an extra column \code{ir} with the ionic
-#' ratio.
+#' A tibble similar to the input \code{data} but with an extra column \code{ir}
+#' with the calculated ionic ratio.
 #'
 #' @examples
 #' \dontrun{
@@ -36,9 +40,7 @@
 #'   collect()
 #'
 #'   # compute ionic ratio and add as new field
-#'   mydata_vw <- calc_ir(mydata)
-#'   # this dataset can be used to make a Van Wirdum diagram with
-#'   # ggplot_vanwirdum_background()
+#'   mydata_with_ir <- calculate_ir(mydata)
 #'
 #' }
 #'
@@ -56,43 +58,62 @@
 #' @importFrom tidyr
 #' pivot_wider
 
-calc_ir <- function(x){
+calculate_ir <- function(data){
 
     # collect the data if needed
-    if ("tbl_sql" %in% class(x)) {
-        x <- x %>%
+    if (inherits(data, "tbl_sql")) {
+        data <- data %>%
             collect
     }
 
-    x <- x %>%
-        filter(.data$chem_variable %in% c("Ca", "Cl", "CondL"))
+    assert_that(all(c("loc_code", "chem_variable",
+                      "value", "unit", "date") %in% colnames(data)),
+                msg = "data does not have the necessary 'loc_code', 'chem_variable', 'value', 'unit' and 'date' columns.")
+
+    assert_that((data %>% filter(.data$chem_variable == "Ca") %>% nrow() ==
+                     data %>% filter(.data$chem_variable == "Cl") %>% nrow() &
+                     data %>% filter(.data$chem_variable == "Cl") %>% nrow() > 0),
+                msg = "there are missing observations for the concentrations of Ca and/or Cl.")
+
+    data_ca_cl <- data %>%
+        select("loc_code", "date", "chem_variable", "value", "unit") %>%
+        filter(.data$chem_variable %in% c("Ca", "Cl"))
+    # note: the measurements for Ca and Cl are always > LOQ
 
     # check units
     # the concentrations in the dataset should be all in meq or all in mg,
     # not in both units
 
-    assert_that(x %>%
-                    filter(.data$chem_variable %in% c("Ca", "Cl")) %>%
+    assert_that(data_ca_cl %>%
                     select("unit") %>%
                     unique() %>%
                     nrow() == 1,
-                msg = "please use the same units for Ca and Cl")
+                msg = "please use the same units for Ca and Cl.")
 
     # calculate ir
 
-    # apparently there can be several results for the same variable and sample id
+    # apparently there can be several results for the same loc_code, chem_variable and date
     # we take the average
-    # note: the measurements for Ca and Cl are always > LOQ
-    x <- x %>%
-        summarize(value = mean(.data$value, na.rm = TRUE),
-                  .by = c("loc_code", "date", "lab_project_id",
-                          "lab_sample_id", "chem_variable",
-                          "unit", "below_loq", "loq", "elneutr"))
+    if (data_ca_cl %>%
+        summarize(n = n(),
+                  .by = c("loc_code", "date",
+                          "chem_variable", "unit")) %>%
+        filter(n > 1) %>%
+        nrow > 0
+        ) {
+        data_ca_cl <- data_ca_cl %>%
+            summarize(value = mean(.data$value, na.rm = TRUE),
+                      .by = c("loc_code", "date",
+                              "chem_variable", "unit"))
+        warning("There are several results for the same 'loc_code', 'chem_variable' and 'date'.
+Is it as expected?
+The ionic ratio will be calculated based on the average of the available measurements, but you might want to check your data.")
+    }
 
-    if ("mg/l" %in% unique(x$unit)) {
+    if ("mg/l" %in% unique(data_ca_cl$unit)) {
         # mg
-        x <- x %>%
-            select(-"unit", -"below_loq", -"loq") %>%
+        data_ca_cl <- data_ca_cl %>%
+            select(-"unit") %>%
             pivot_wider(names_from = "chem_variable", values_from = "value") %>%
             mutate(Ca_meq = (.data$Ca*2)/40.078,
                    Cl_meq = .data$Cl/35.453,
@@ -100,14 +121,19 @@ calc_ir <- function(x){
 
     } else {
         # meq
-        x <- x %>%
-            select(-"unit", -"below_loq", -"loq") %>%
+        data_ca_cl <- data_ca_cl %>%
+            select(-"unit") %>%
             pivot_wider(names_from = "chem_variable", values_from = "value") %>%
             mutate(ir = .data$Ca/(.data$Ca + .data$Cl))
 
     }
 
-    return(x)
+    data <- data %>%
+        left_join(data_ca_cl %>%
+                      select("loc_code", "date", "ir"),
+                  by = c("loc_code", "date"))
+
+    return(data)
     # with ir without units (0-1)
 
 }
