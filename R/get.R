86dc2ab5e1316b08054e032c61509347465b1016
Droomelot De Gendt <droomelot.degendt@inbo.be>
2026-08-31 17:58:29 +0200

    feat(get_chem): use 'StaalExterneSource' as lab_project_id

--- R/get.R
+++ R/get.R
@@ -1172,14 +1172,13 @@ get_chem <- function(
     chemdata %>%
     # temporary values:
     mutate(
-      lab_project_id = "0",
       lab_sample_id = sql("CAST(StaalID AS varchar)"),
       loq = -99
     ) %>%
     select(
       .data$loc_code,
       date = .data$Datum,
-      .data$lab_project_id,
+      lab_project_id = .data$StaalExterneSource,
       .data$lab_sample_id,
       chem_variable = .data$ChemVarCode,
       value_mass = .data$Meetwaarde,
@@ -1391,7 +1390,8 @@ build_chem_query <- function(
         select(
           .data$StaalWID,
           .data$StaalElnNbr,
-          .data$ENCalculated
+          .data$ENCalculated,
+          .data$StaalExterneSource
         ),
       by = "StaalWID"
     )
